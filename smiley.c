ae310c9ba3feeef7c978dfe45e697dc5e57094b0
sarab <sarabpal.maan@gmail.com>
2025-11-16 13:19:52 -0800

    Add initial files for dirtypageflags exploit

---
+++ smiley.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("This file has been successfully replaced\n");
+    printf("\n");
+    printf("        _ _ _ _ _ _ _ _ _\n");
+    printf("       /                 \
+");
+    printf("      /   _ _ _ _ _ _ _   \
+");
+    printf("     |   /           \   |\n");
+    printf("     |  |   _     _   |  |\n");
+    printf("     |  |  (.)   (.)  |  |\n");
+    printf("     |  |     <       |  |\n");
+    printf("     |  |   '-----'   |  |\n");
+    printf("     |   \___________/   |\n");
+    printf("      \                 /\n");
+    printf("       \ _ _ _ _ _ _ _ /\n");
+    printf("\n");
+    return 0;
+}
